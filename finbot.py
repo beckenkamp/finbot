2518a2edd19c75b27a4dcaea86ac7ffdd3efdb0b
Marcus Beckenkamp <mvbeck@gmail.com>
2016-11-25 15:05:16 -0200

    Change file name

--- finbot.py
+++ finbot.py
@@ -1,25 +1,26 @@
 from flask import Flask, request
+import os
 import requests
 import json
 import traceback
 import random
 app = Flask(__name__)
 
-token = "<ACCESS_TOKEN_HERE>"
+token = os.environ.get('FB_ACCESS_TOKEN')
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
   if request.method == 'POST':
     try:
-      data = json.loads(request.data)
+      data = json.loads(request.data.decode())
       text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
       sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
       payload = {'recipient': {'id': sender}, 'message': {'text': "Hello World"}} # We're going to send this back
       r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload) # Lets send it
     except Exception as e:
-      print traceback.format_exc() # something went wrong
+      print(traceback.format_exc())  # something went wrong
   elif request.method == 'GET': # For the initial verification
-    if request.args.get('hub.verify_token') == '<VERIFY_TOKEN_HERE>':
+    if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
       return request.args.get('hub.challenge')
     return "Wrong Verify Token"
   return "Hello World" #Not Really Necessary
