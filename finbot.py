b6dd4c1467b0555d79d5a6e4b0a43a943b4183ae
Marcus Beckenkamp <mvbeck@gmail.com>
2016-11-26 17:35:29 -0200

    Typing on signal and fixes in database and model

--- finbot.py
+++ finbot.py
@@ -4,6 +4,7 @@ import json
 import traceback
 import random
 
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.mysql import BIGINT
 from flask import Flask, request
@@ -16,10 +17,6 @@ db_file = os.path.realpath('finbot.db')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(db_file)
 db = SQLAlchemy(app)
-
-# Verify if database exists and create it
-if not os.path.isfile(db_file):
-    db.create_all()
 
 
 class User(db.Model):
@@ -34,9 +31,11 @@ class User(db.Model):
     locale = db.Column(db.String(20))
     timezone = db.Column(db.Integer)
     gender = db.Column(db.String(20))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime, default=datetime.now())
 
-    def __init__(self, first_name):
-        self.first_name = first_name
+    def __init__(self):
+        self.created_at = datetime.now()
 
     def __repr__(self):
         return '{} {}'.format(self.first_name, self.last_name)
@@ -69,6 +68,14 @@ def get_or_create_user(sender):
     return user
 
 
+def send_loading_message(sender):
+    """
+    Sends a signal to messenger informing is loading the data
+    """
+    payload = {'recipient': {'id': sender}, 'sender_action': 'typing_on'}
+    requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
+
+
 @app.route('/')
 def index():
     return 'Finbot'
@@ -78,12 +85,16 @@ def webhook():
     if request.method == 'POST':
         try:
             data = json.loads(request.data.decode())
-            text = data['entry'][0]['messaging'][0]['message']['text'] # Incoming Message Text
-            sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
+            text = data['entry'][0]['messaging'][0]['message']['text']  # Incoming Message Text
+            sender = data['entry'][0]['messaging'][0]['sender']['id']  # Sender ID
 
+            send_loading_message(sender)  # Typing on signal
             user = get_or_create_user(sender)
 
-            payload = {'recipient': {'id': sender}, 'message': {'text': "Hello, {}".format(user.first_name)}}
+            message = "Hello, {}. A data de atualização das suas informações é {}".format(user.first_name,
+                user.updated_at.strftime('%d/%m/%Y %H:%M'))
+
+            payload = {'recipient': {'id': sender}, 'message': {'text': message}}
             r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
         except Exception as e:
             print(traceback.format_exc())  # something went wrong
@@ -91,6 +102,10 @@ def webhook():
         if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
             return request.args.get('hub.challenge')
         return "Wrong Verify Token"
+    return "Nothing"
 
 if __name__ == '__main__':
+    # Verify if database exists and create it
+    if not os.path.isfile(db_file):
+        db.create_all()
     app.run(debug=True)
